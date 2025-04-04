34777c4987ac347e73a87fabb27282cd843d2143
tony <tony@woo.network>
2025-04-04 22:47:18 +0800

    fix

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -9,7 +9,7 @@ export default function DefaultLayout({
   return (
     <div className=" bg-white relative flex flex-col h-screen mx-auto max-w-7xl">
       <Header />
-      <div className="w-[200px] bg-default-600 text-white bottom-0 absolute left-0 top-16">
+      <div className="w-[200px] bg-default-600 text-white bottom-0 absolute left-0 top-16 z-[2]">
         <Sidebar />
       </div>
       <div className="relative">
