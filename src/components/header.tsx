32efe176d10c875fdd4b6c3f042717177e6a3731
tony <tony@woo.network>
2025-04-15 22:48:03 +0800

    fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
     <div className="h-16 bg-[#4A3AFF]  w-full flex justify-between items-center px-5 z-[5]">
       <p className="text-[28px] text-white font-[400]">卓敏健康管理后台程序</p>
       <div className="flex items-center gap-4">
-        <Button color="danger" variant="ghost" onPress={() => {
+        <Button color="default" className="text-white" variant="light" onPress={() => {
           useAuthStore.getState().clearToken();
           navigate('/login');
         }}>退出</Button>
